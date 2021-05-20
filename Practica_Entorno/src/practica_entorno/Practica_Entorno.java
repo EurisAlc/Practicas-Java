a5cfeb8513e5315445665292c0c4dc3566cf7d13
Euris Alcántara <57092486+EurisAlc@users.noreply.github.com>
2021-05-20 10:47:49 +0200

    Update Practica_Entorno.java

--- Practica_Entorno/src/practica_entorno/Practica_Entorno.java
+++ Practica_Entorno/src/practica_entorno/Practica_Entorno.java
@@ -8,23 +8,23 @@ import java.util.Scanner;
  * Programa que almacena 10 números por teclado sin decimales 
  * para después devolverlos en orden ascendente. También, se 
  * deben obtener los números pares que son divisibles por 2 y
- * los impares y mostrarlos en orden descendente.
+ * los impares mostrados en orden descendente.
  * Versión 1.0
  * @author Euris Alc.
  */
 public class Practica_Entorno {
 
-    /**
+    // Declaración de variables.
+    int[] numeros = new int[10];
+    boolean repetir = false;
+    Scanner teclado_int = new Scanner(System.in);
+    
+     /**
      * Primer método: Controla los valores intrucidos por teclado 
      * por medio de un bucle do-while y las posibles excepciones 
      * en el bloque try-catch.
      * @param array Array para acceder a los elementos.
      */
-    // Declaración de variables.
-    int[] numeros = new int[10];
-    boolean repetir = false;
-    Scanner teclado_int = new Scanner(System.in);
-
     public void IngresarNumero() {
         do {
             /**
@@ -35,7 +35,7 @@ public class Practica_Entorno {
                 System.out.println("------------------------------");
                 System.out.println("Introduzca 10 números enteros.");
                 System.out.println("------------------------------");
-                // Recorre los elemtos introducidos por medio del bucle for.
+                // Recorre los elementos introducidos por medio del bucle for.
                 for (int i = 0; i < numeros.length; ++i) {
                     System.out.printf("Valor: ", i + 1);
                     numeros[i] = teclado_int.nextInt();
@@ -58,13 +58,12 @@ public class Practica_Entorno {
     public void OrdenarNumeros() {
         Arrays.sort(numeros);
         System.out.println("-----------------------------------------------------------------------------");
-        System.out.println("Orden ascendente de los números introducidos: "
-                + Arrays.toString(numeros));
+        System.out.println("Orden ascendente de los números introducidos: " + Arrays.toString(numeros));
         System.out.println("-----------------------------------------------------------------------------");
     }
      /**
-     * Tercer método: Divide los valores del array en 2,
-     * clasifica los que son parese e impares y los ordena
+     * Tercer método: Divide por dos los valores almacenados en el
+     * array, clasifica los que son parese e impares y los ordena
      * en orden descendente.
      */
     public void Division(){      
@@ -89,6 +88,3 @@ public class Practica_Entorno {
         rest.Division();
     }
 }
-
-
-
